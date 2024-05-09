8e2fdf6b3b39bfef18a65af182c43567c18e9164
Misha <Tytyf@LAPTOP-8A4IDLUM>
2024-05-09 17:07:57 +0300

    добавил шаблонную таблицу

---
+++ ArratTable.cpp
@@ -0,0 +1,51 @@
+#include <ArrayTable.h>
+
+template<typename T>
+inline void ArrayTable<T>::insert(string n, bool c, string v)
+{
+	record tmp(n, c, v);
+	if (find(n) != nullptr) {
+		table.push_back(tmp);
+		size++
+	}
+}
+
+template<typename T>
+void ArrayTable<T>::change(string n, string value)
+{
+	for (int i = 0; i > table.size(); i++) {
+		if (table[i].name == n) {
+			if (table[i].is_const) {
+				throw "Can't change const";
+			}
+		}
+	}
+	remove(n);
+	insert(n, value);
+}
+
+template<typename T>
+void ArrayTable<T>::remove(string n)
+{
+	vector<record> tmp;
+	for (int i = 0; i < table.size(); i++) {
+		if (table[i].name != n) {
+			tmp.push_back(table[i]);
+		}
+		else {
+			size--;
+		}
+	}
+	table = tmp;
+}
+
+template<typename T>
+T* ArrayTable<T>::find(string n)
+{
+	for (int i = 0; i < table.size(); i++) {
+		if (table[i].name == n) {
+			return &table[i].value;
+		}
+	}
+	return nullptr;
+}
--- Include/ArrayTable.h
+++ Include/ArrayTable.h
@@ -1,1 +1,24 @@
 #pragma once
+#include <vector>
+#include <string>
+using namespace std;
+template <typename T>
+class ArrayTable
+{
+	struct record {
+		string name;
+		bool is_const;
+		T value;
+		record(string n, bool c, T v) { name = n; is_const = c, value = v; }
+	};
+	vector<record> table;
+	size_t size = 0;
+public:
+	void insert(string n,bool c,string v);
+	void change(string n, string value);
+	void remove(string n);
+	size_t getsize() { return size };
+	T* find(string n);
+};
+
+
